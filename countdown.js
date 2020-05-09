dfdc337ffee8432e26bdbaa543b17a2f1bf9f933
KannanVS <kannansanthanam97@gmail.com>
2020-05-09 14:07:04 +0200

    countdown clock

---
+++ countdown.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const parseArgs = require("minimist");
+const { stdout: log } = require("single-line-log");
+const Timer = require("tiny-timer");
+const { time } = parseArgs(process.argv);
+
+if (!time) {
+    throw new Error("--time is required");
+}
+
+if (!parseInt(time)) {
+    throw new Error("--time must be number");
+}
+
+const count = parseInt(time);
+let message = "";
+
+for (let i = 0; i < count; i++) {
+    message += '*'
+}
+
+log(message);
+
+const timer = new Timer({ interval: 1000 })
+
+timer.on("tick", () => {
+    log(message);
+    message = message.slice(1);
+})
+
+timer.start(count*1000);
+// console.log(time);
